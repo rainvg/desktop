b893ca5a63ca056024390577a902ddb6e7c8d279
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2016-03-31 17:50:02 +0200

    Minor bugfix.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var path = require('path');
 var potty = require('potty');
 var os = require('os');
 
-var pot = new potty.pot(path.resolve(process.env.HOME || process.env.HOMEPATH, '.rain'), 'https://rain.vg/releases/desktop-daemon/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', {ELECTRON_RUN_AS_NODE: false});
+var pot = new potty(path.resolve(process.env.HOME || process.env.HOMEPATH, '.rain'), 'https://rain.vg/releases/desktop-daemon/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/production/package', {ELECTRON_RUN_AS_NODE: false});
 
 pot.on('shutdown', function()
 {
