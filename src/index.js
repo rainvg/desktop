082b0e702c16b142e5b77e6442e07c11c3793446
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2016-04-05 19:32:21 +0200

    Dirty patching.

--- src/index.js
+++ src/index.js
@@ -10,68 +10,75 @@ var scheme = require('../package.json').scheme;
 if(!(process.env.ELECTRON_RUN_AS_NODE))
 {
   process.env.ELECTRON_RUN_AS_NODE = true;
-  child_process.spawn(process.argv[0], [__filename], {detached: true, stdio: ['inherit', 'inherit', 'inherit']});
-  require('electron').app.exit(0);
-}
+  var child = child_process.spawn(process.argv[0], [__filename], {detached: true, stdio: ['ignore', 'ignore', 'ignore']});
+  child.unref();
 
-var __date__ = function()
-{
-  var __fill__ = function(data)
+  setTimeout(function()
   {
-    return (data < 10 ? '0' : '') + data;
+    require('electron').app.exit(0);
+  }, 10000);
+}
+else
+{
+  var __date__ = function()
+  {
+    var __fill__ = function(data)
+    {
+      return (data < 10 ? '0' : '') + data;
+    };
+
+    var date = new Date();
+
+    var result = date.getUTCFullYear();
+    result += __fill__(date.getUTCMonth() + 1);
+    result += __fill__(date.getUTCDate());
+    result += __fill__(date.getUTCHours());
+    result += __fill__(date.getUTCMinutes());
+
+    return result;
   };
 
-  var date = new Date();
+  var rain_path = {root: path.resolve(os.homedir(), '.rain')};
+  rain_path.lockfile = path.resolve(rain_path.root, 'singleton.lock');
 
-  var result = date.getUTCFullYear();
-  result += __fill__(date.getUTCMonth() + 1);
-  result += __fill__(date.getUTCDate());
-  result += __fill__(date.getUTCHours());
-  result += __fill__(date.getUTCMinutes());
+  rain_path.log = {};
+  rain_path.log.folder = path.resolve(rain_path.root, 'logs');
+  rain_path.log.file = path.resolve(rain_path.log.folder, __date__());
 
-  return result;
-};
+  if(!fs.existsSync(rain_path.root))
+    fs.mkdirSync(rain_path.root);
 
-var rain_path = {root: path.resolve(os.homedir(), '.rain')};
-rain_path.lockfile = path.resolve(rain_path.root, 'singleton.lock');
-
-rain_path.log = {};
-rain_path.log.folder = path.resolve(rain_path.root, 'logs');
-rain_path.log.file = path.resolve(rain_path.log.folder, __date__());
-
-if(!fs.existsSync(rain_path.root))
-  fs.mkdirSync(rain_path.root);
-
-sheriff.lock(rain_path.lockfile).then(function()
-{
-  if(scheme === 'development')
+  sheriff.lock(rain_path.lockfile).then(function()
   {
-    if(!fs.existsSync(rain_path.log.folder))
-      fs.mkdirSync(rain_path.log.folder);
-
-    var __oldout__ = process.stdout.write;
-
-    process.stdout.write = function(data)
+    if(scheme === 'development')
     {
-      __oldout__.apply(this, arguments);
-      fs.appendFileSync(rain_path.log.file, data);
-    };
-  }
+      if(!fs.existsSync(rain_path.log.folder))
+        fs.mkdirSync(rain_path.log.folder);
 
-  var pot = new potty(path.resolve(process.env.HOME || process.env.HOMEPATH, '.rain'), 'https://rain.vg/releases/desktop-daemon/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/' + scheme + '/package', {ELECTRON_RUN_AS_NODE: false, log: console.log});
+      var __oldout__ = process.stdout.write;
 
-  pot.on('shutdown', function()
+      process.stdout.write = function(data)
+      {
+        __oldout__.apply(this, arguments);
+        fs.appendFileSync(rain_path.log.file, data);
+      };
+    }
+
+    var pot = new potty(path.resolve(process.env.HOME || process.env.HOMEPATH, '.rain'), 'https://rain.vg/releases/desktop-daemon/' + os.type().toLowerCase() + '-' + os.arch().toLowerCase() + '/' + scheme + '/package', {ELECTRON_RUN_AS_NODE: false, log: console.log});
+
+    pot.on('shutdown', function()
+    {
+      process.exit();
+    });
+
+    pot.start();
+  });
+
+  process.once('SIGINT', function ()
+  {
+    process.exit();
+  }).once('SIGTERM', function ()
   {
     process.exit();
   });
-
-  pot.start();
-});
-
-process.once('SIGINT', function ()
-{
-  process.exit();
-}).once('SIGTERM', function ()
-{
-  process.exit();
-});
+}
